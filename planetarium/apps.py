c581e36da338f112db71a824141c53a8acbd7c98
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-12-26 15:01:41 +0200

    Add signals for redis

--- planetarium/apps.py
+++ planetarium/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PlanetariumConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "planetarium"
+
+    def ready(self):
+        import planetarium.signals
