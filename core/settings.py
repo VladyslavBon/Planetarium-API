c2df29c2a0e22dbaf310332b9e53b0b72ec22418
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-12-26 15:06:24 +0200

    Hide secret key django

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4-a!zj+wdf*e(#7_jfn3^ys0#*7$^5(d0)dz^#a0j69y#ic)h6"  # os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
