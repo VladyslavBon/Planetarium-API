573b247d9efee73044148201b3c25ec2495e94c4
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-12-30 13:17:29 +0200

    Changed image_field_path

--- planetarium/utils.py
+++ planetarium/utils.py
@@ -1,11 +1,11 @@
-import pathlib
+import os
 import uuid
 
 from django.utils.text import slugify
 
 
 def image_file_path(instance, filename):
-    filename = (
-        f"{slugify(instance.title)}-{uuid.uuid4()}" + pathlib.Path(filename).suffix
-    )
-    return pathlib.Path("uploads/astronomy_show/") / pathlib.Path(filename)
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/astronomy_show/", filename)
