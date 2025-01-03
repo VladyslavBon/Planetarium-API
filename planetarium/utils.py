ab5de843a14689a0f935eccc71f78160e7c62132
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2025-01-03 15:17:39 +0200

    Update os.path.join params

--- planetarium/utils.py
+++ planetarium/utils.py
@@ -8,4 +8,4 @@ def image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/astronomy_show/", filename)
+    return os.path.join("uploads", "astronomy_show", filename)
